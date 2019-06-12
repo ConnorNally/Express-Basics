c7403c74db4b805899009fd3e382322371c43183
ConnorNally <51028870+ConnorNally@users.noreply.github.com>
2019-06-12 14:18:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const name = req.cookies.username 
+  if (name) {
+  res.render("index", { name });
+  } else {
+    res.redirect('/hello');
+  }
 });
 
 app.get("/cards", (req, res) => {
@@ -13,11 +22,26 @@ app.get("/cards", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-    res.render("hello");
-  });
+  res.render("hello");
+});
+
+app.post("/goodbye", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/hello');
+});
 
 app.post("/hello", (req, res) => {
-res.render("hello");
+  res.cookie('username', req.body.username);
+  res.redirect("/");
+});
+
+app.get("/hello", (req, res) => {
+  const name = req.cookies.username 
+  if (name) {
+  res.redirect("/", { name });
+  } else {
+    res.render('hello');
+  }
 });
 
 
